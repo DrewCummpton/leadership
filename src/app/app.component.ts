e42ff7c8f2b2023cc5338227a88202c667af70d6
Drew Cumpton <dcumpto@wgu.edu>
2024-09-23 07:15:12 -0600

    add json pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PeopleService } from '../shared/people.service';
+import { JsonPipe } from '@angular/common';
  
 @Component({
   standalone: true,
-  imports: [],
+  imports: [ JsonPipe ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
